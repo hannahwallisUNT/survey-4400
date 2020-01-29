46383e6ffc731bc290d57645ef3ce91bf2d6b96e
hannahwallisUNT <hannahwallisUNT@gmail.com>
2020-01-29 10:18:05 -0600

    Fixed spacing and changed landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :surveys
-  root 'surveys#index'
+  root 'surveys#new'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
